42e3e02156a34e78724e4d8d1e2caea2420a4616
Łukasz <lukaszkus77@gmail.com>
2024-07-25 02:44:08 +0200

    update

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -12,7 +12,7 @@ const handle = async (req, res) => {
   await isAdminRequest(req, res);
   const form = new multiparty.Form();
   const { fields, files } = await new Promise((resolve, reject) => {
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, async (err, fields, files) => {
       if (err) reject(err);
       resolve({ fields, files });
     });
@@ -22,7 +22,7 @@ const handle = async (req, res) => {
     region: "us-east-1",
     credentials: {
       accessKeyId: process.env.S3_ACCESS_KEY,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_KEY,
     },
   });
   const links = [];
